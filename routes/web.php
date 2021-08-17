2eeaf08eea50ba7a5cbd1e4eb0186b058e32fa4e
Sunil <sunil.khadka001@gmail.com>
2021-08-17 08:11:37 +0545

    routing practice

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', function () {
+    echo "boom";
+});
+
+Route::get('/project', function () {
+    echo "in project";
+});
+
+Route::get('/harry', function () {
+    // enter the name of file before .blade.php;
+    return view('harry');
+});
+
+Route::get('/sherlock', function () {
+    // to access file inside a folder = foldername.filename
+    return view('sherlock.sherlock');
+});
+
+
+
+// available functions
+
+// Get = reads
+// Route::get();
+
+// POST = store/creat
+// Route::post();
+
+// Put = update
+// Route::put();
+
+// delete = delete
+// Route::delete();
+// Route::patch();
